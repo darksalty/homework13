1d985f949f7c7388e307372f51b42b092490c594
Evgenii Rasolko <grimnosh@mail.ru>
2020-07-02 20:03:55 +0300

    homework13

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+var a = prompt('0 < .. < 5');
+
+if (a == 1)
+  alert('Правильно')
+else if (a == 2)
+  alert('Правильно')
+else if (a == 3)
+  alert('Правильно')
+else if (a == 4)
+  alert('Правильно')
+else alert('Неправильно');
---
+++ assets/js/swith.js
@@ -0,0 +1,22 @@
+var num = prompt("Введите число?");
+switch (num) {
+  
+  case '1':
+    alert( 'Зима' );
+    break;
+
+  case '2':
+    alert( 'Весна' );
+    break;
+
+  case '3':
+    alert('Лето');
+    break;
+
+  case '4':
+    alert('Осень');
+    break;
+
+  default:
+    alert( 'Неизвестное значение' );
+}
